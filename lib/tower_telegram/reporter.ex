898f92815c8770211bd4f0e83dd8138b886ca453
Iván Hernández Cazorla <ivan@corujadigital.tech>
2024-10-05 02:22:16 +0100

    apply mix format

--- lib/tower_telegram/reporter.ex
+++ lib/tower_telegram/reporter.ex
@@ -7,6 +7,7 @@ defmodule TowerTelegram.Reporter do
     if Tower.equal_or_greater_level?(level, level()) do
       do_report_event(event)
     end
+
     :ok
   end
 
@@ -14,36 +15,35 @@ defmodule TowerTelegram.Reporter do
     Telegex.send_message(
       chat_id(),
       build_message(e),
-      parse_mode: "markdown")
+      parse_mode: "markdown"
+    )
+
     :ok
   end
 
   defp build_message(%Tower.Event{
-    kind: kind,
-    reason: exception,
-    stacktrace: stacktrace
-  }) do
+         kind: kind,
+         reason: exception,
+         stacktrace: stacktrace
+       }) do
     """
     Tower received an error of kind `#{kind}`.
 
     *Reason*:
     ```elixir
-    #{inspect exception}
+    #{inspect(exception)}
     ```
     *Stacktrace*:
     ```elixir
-    #{inspect stacktrace}
+    #{inspect(stacktrace)}
     ```
     #{include_hashtags(kind)}
     """
   end
 
-  defp include_hashtags(kind), do:
-  "#tower\\_report #tower\\_type\\_#{kind}"
+  defp include_hashtags(kind), do: "#tower\\_report #tower\\_type\\_#{kind}"
 
-  defp level(), do:
-  Application.get_env(:tower_telegram, :level, @default_level)
+  defp level(), do: Application.get_env(:tower_telegram, :level, @default_level)
 
-  defp chat_id(), do:
-  Application.get_env(:tower_telegram, :chat_id)
+  defp chat_id(), do: Application.get_env(:tower_telegram, :chat_id)
 end
