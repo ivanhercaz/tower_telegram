a4e44063ab7a081c95610aa3d781a94d5c5bf243
Iván Hernández Cazorla <ivan@corujadigital.tech>
2024-10-05 02:10:59 +0100

    implement a minimal reporter

--- lib/tower_telegram/reporter.ex
+++ lib/tower_telegram/reporter.ex
@@ -7,18 +7,43 @@ defmodule TowerTelegram.Reporter do
     if Tower.equal_or_greater_level?(level, level()) do
       do_report_event(event)
     end
+    :ok
   end
 
-  def do_report_event(%Tower.Event{
+  def do_report_event(%Tower.Event{} = e) do
+    Telegex.send_message(
+      chat_id(),
+      build_message(e),
+      parse_mode: "markdown")
+    :ok
+  end
+
+  defp build_message(%Tower.Event{
     kind: kind,
-    id: id,
     reason: exception,
     stacktrace: stacktrace
   }) do
-    # TODO: report event with Telegex
+    """
+    Tower received an error of kind `#{kind}`.
+
+    *Reason*:
+    ```elixir
+    #{inspect exception}
+    ```
+    *Stacktrace*:
+    ```elixir
+    #{inspect stacktrace}
+    ```
+    #{include_hashtags(kind)}
+    """
   end
 
-  defp level() do
-    Application.get_env(:tower_telegram, :level, @default_level)
-  end
+  defp include_hashtags(kind), do:
+  "#tower\\_report #tower\\_type\\_#{kind}"
+
+  defp level(), do:
+  Application.get_env(:tower_telegram, :level, @default_level)
+
+  defp chat_id(), do:
+  Application.get_env(:tower_telegram, :chat_id)
 end
