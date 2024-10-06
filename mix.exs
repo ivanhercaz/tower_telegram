65e3a489d559af7e677524d40d8b4d54dcc2f35b
Iván Hernández Cazorla <ivan@corujadigital.tech>
2024-10-06 16:01:53 +0100

    release version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TowerTelegram.MixProject do
   def project do
     [
       app: :tower_telegram,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       package: package(),
